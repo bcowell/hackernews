7e87268436854f2009bbd890487986a6dae370f6
Brayden Cowell <bcowell@uoguelph.ca>
2018-09-01 18:46:11 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+// temp data, later fetch this from API
+const stories = [
+    {
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+];
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
